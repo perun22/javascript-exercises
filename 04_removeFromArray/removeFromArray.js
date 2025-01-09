997c44dcf3b4b040b7d5fb1ac8ca860023fb6c83
Suzana <100297163+perun22@users.noreply.github.com>
2025-01-09 17:46:04 +0100

    Add code and pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+        const newArray = [];
+        array.forEach((item) => {
+            if (!itemsToRemove.includes(item)) {
+                newArray.push(item);
+            }
+        })
+    return newArray;
 };
 
 // Do not edit below this line
