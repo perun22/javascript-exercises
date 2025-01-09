aca35bb30f4aace68a8e362442fb0226b22355bd
Suzana <100297163+perun22@users.noreply.github.com>
2025-01-09 19:17:52 +0100

    Add code and test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f - 32) * 5/9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = c * 9/5 + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
