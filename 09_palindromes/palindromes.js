9936a2c2aaa3214638c5a0296514344e23c8bd17
Suzana <100297163+perun22@users.noreply.github.com>
2025-04-09 20:06:00 +0200

    Solve exercise and pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanStr = str.toLowerCase().match(/[a-z0-9]/g);
+  let adjustedStr = cleanStr.join('');
+  let reversedStr = cleanStr.reverse().join('');
 
+  if (reversedStr === adjustedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
