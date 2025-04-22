b9637baef03eb9bebb27e6af134f871ff592eb7c
Suzana <100297163+perun22@users.noreply.github.com>
2025-04-22 12:26:05 +0200

    Add code and pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = (input) => {
+  let num;
+  if (typeof input !== 'number') {
+    num = parseInt(input);
+  } else {
+    num = input;
+  }
 
+  if (num < 0) return 'OOPS';
+  if (num == 0) return 0;
+
+  const sequence = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence[num];
 };
 
 // Do not edit below this line
